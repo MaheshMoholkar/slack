61770e123b18534d7ba9f8f157a346475632b73b
Mahesh Moholkar <moholkarmahesh@aca.edu.in>
2025-05-09 20:49:59 +0530

    add user button

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,7 +1,18 @@
 import GitHub from "@auth/core/providers/github";
 import { convexAuth } from "@convex-dev/auth/server";
-import { Password } from "@convex-dev/auth/providers/password";
+import { Password } from "@convex-dev/auth/providers/Password";
+
+import { DataModel } from "../convex/_generated/dataModel";
+
+const CustomePassword = Password<DataModel>({
+  profile(params) {
+    return {
+      name: params.name as string,
+      email: params.email as string,
+    };
+  },
+});
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [GitHub, Password],
+  providers: [GitHub, CustomePassword],
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { UserButton } from "@/features/auth/components/user-button";
 import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
@@ -7,9 +8,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl font-bold">Welcome to Convex</h1>
-      <p className="mt-4 text-lg">
-        This is a simple example of using Convex with Next.js.
-      </p>
+      <UserButton />
       <button
         onClick={() => signOut()}
         className="mt-6 px-4 py-2 text-white bg-blue-600 rounded"
